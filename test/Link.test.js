eb08f5975e6f95f151d9e60dc16437554382f0f3
Trey Shugart <treshugart@gmail.com>
2017-09-26 21:47:49 +1000

    Fix tests.

--- test/Link.test.js
+++ test/Link.test.js
@@ -6,13 +6,11 @@ import cases from 'jest-in-case';
 
 import { Link } from '../src';
 
-test('render', () => {
-  cases('add(augend, addend)', opts => {
-    return mount(dom).wait(w => {
-      expect(w.node.innerHTML).toMatchSnapshot();
-    });
-  }, [
-    <Link />,
-    <Link href="/test" />
-  ]);
-});
+cases('render', dom => {
+  return mount(dom).wait(w => {
+    expect(w.node.innerHTML).toMatchSnapshot();
+  });
+}, [
+  <Link />,
+  <Link href="/test" />
+]);
