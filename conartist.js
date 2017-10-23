67d35cc828b2e573399c7429ea57801d3d44c6af
Trey Shugart <treshugart@gmail.com>
2017-10-23 20:38:32 +1100

    Fix build.

--- conartist.js
+++ conartist.js
@@ -1,3 +1,18 @@
 const { config, preset } = require('conartist');
 
-module.exports = config(preset.babel(), preset.base(), preset.jest());
+module.exports = config(
+  preset.babel(),
+  preset.base(),
+  preset.jest(),
+  preset.rollup(),
+  {
+    'rollup.config.js'() {
+      return Object.assign(
+        {
+          name: 'skRouter'
+        },
+        preset.rollup()['rollup.config.js']()
+      );
+    }
+  }
+);
